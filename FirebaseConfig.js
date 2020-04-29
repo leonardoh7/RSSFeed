5426bca52375fb758d2e46ba1e6168fc2defd124
Leo <hoangtt995@gmail.com>
2020-04-29 12:25:34 -0400

    remove dotenv, implement react navigation, add FirebaseConfig.js

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,17 +1,17 @@
 import * as firebase from 'firebase';
 import '@firebase/firestore';
 
-
 var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyCkFsaY2smwvPBtLiog4dW58TCM7RBJXyY",
+  authDomain: "rssfeed-4e916.firebaseapp.com",
+  databaseURL: "https://rssfeed-4e916.firebaseio.com",
+  projectId: "rssfeed-4e916",
+  storageBucket: "rssfeed-4e916.appspot.com",
+  messagingSenderId: "701655634053",
+  appId: "1:701655634053:web:72c308ccae21848ab399ec"
 };
 
+console.log(process.env.AUTH_DOMAIN);
 let app = firebase.initializeApp(firebaseConfig);
 
 export const db = app.database();
